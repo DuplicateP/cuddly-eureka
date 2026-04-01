d87719037270ec4a80181382847b3d9cf394464a
DuplicateP <modassirjawaid44@gmail.com>
2026-04-01 14:40:03 +0530

    Update API credentials and owner details

--- vars.py
+++ vars.py
@@ -3,17 +3,17 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28389286"))
-API_HASH = environ.get("API_HASH", "b88da5f4f338cca30f8ea5fb53cb083b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8304937637:AAGl0NXbjoxceaUXxpbiIyGuc2rn1TrsCds")
+API_ID = int(environ.get("API_ID", "26792945"))
+API_HASH = environ.get("API_HASH", "6be4bc36c6a70c269f80a7388e59e150")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "6334323103"))
-CREDIT = environ.get("CREDIT", "💫『 𝒟𝒾𝓋𝓎𝒶𝓃𝓈𝒽 𝓈𝒽𝓊𝓀𝓁𝒶 』💫")
+OWNER = int(environ.get("OWNER", "7357284659"))
+CREDIT = environ.get("CREDIT", "💫『 𝗠𝗢𝗗𝗔𝗦𝗦𝗜𝗥 』💫")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8056097370').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7357284659').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8056097370').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7357284659').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
